32e4ec25175b344fef3b4c346ebdbd01d217bf75
Koji Morita <kmorita@gungho.jp>
2018-01-16 14:54:15 +0900

    main2 add
    main2 add

---
+++ main2/java/org/t246osslab/easybuggy4sb/troubles/InfiniteLoopController.java
@@ -0,0 +1,23 @@
+package org.t246osslab.easybuggy4sb.troubles;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class InfiniteLoopController {
+
+    private static final Logger log = LoggerFactory.getLogger(InfiniteLoopController.class);
+
+    @RequestMapping(value = "/infiniteloop")
+    public void process(HttpServletRequest req) {
+        while (true) {
+            String contextPath = req.getContextPath();
+            int contentLength = req.getContentLength();
+            log.debug("contextPath: {}, contentLength: {}", contextPath, contentLength);
+        }
+    }
+}
